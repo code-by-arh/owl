b4a67bae1e7a93c6b1b3571c4dbb668704d78346
arhgit81 <aridwan2012@gmail.com>
2025-09-22 15:56:35 +0700

    [IMP] Update Quiz Range Field Widget

--- widget_range_field/models/partner.py
+++ widget_range_field/models/partner.py
@@ -9,5 +9,8 @@ class partner(models.Model):
 
 
     salary = fields.Integer("Salary")
+    min_value = fields.Integer("Minimum", default=100)
+    max_value = fields.Integer("Maximum", default=10000)
+    step_value = fields.Integer("Step", default=100)
 
 
